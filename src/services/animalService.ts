6e8825c48fad9a2d3ccb38a47b8c2dbbe1e5d1db
Malin <maa.lin@outlook.com>
2025-06-09 19:38:06 +0200

    feat: Add Animal interface and add IAnimal as type in services

--- src/services/animalService.ts
+++ src/services/animalService.ts
@@ -1,15 +1,16 @@
+import type { IAnimal } from "../models/IAnimal";
 import { get } from "./baseService";
 
 const BASE_URL = "https://animals.azurewebsites.net/api/animals";
 
 export const getAnimals = async () => {
-    const response = get(BASE_URL);
+    const response = get<IAnimal[]>(BASE_URL);
 
     return response;
 };
 
 export const getAnimal = async (id: number) => {
-    const response = get(BASE_URL + id);
+    const response = get<IAnimal>(BASE_URL + id);
 
     return response;
 };
